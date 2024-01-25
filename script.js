a4221e4c823662a4be01a5524df6d482df268b5c
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-25 19:38:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// write js code here if required
+function clock(){
+
+	let today = new Date();
+	let month = today.getMonth()+1;
+	let day = today.getDate();
+	let year = today.getFullYear();
+	let hours = today.getHours();
+	
+	let minutes = today.getMinutes();
+	if(minutes<10)
+	{
+		minutes = "0"+minutes;
+	}
+
+	let seconds = today.getSeconds();
+	if(seconds<10){
+		seconds = "0"+seconds;
+	}
+	
+	let time = document.getElementById("timer");
+	time.innerHTML = day+"/"+ month+"/"+ year +"," + " " + hours 
+		+":"+ minutes + ":" + seconds;
+
+}
